47f9366b41b632bcea9c04fd74d04f7146574f7e
Willian Costa De Castro <willian.castro.ext@tivit.com>
2019-07-24 11:16:50 -0300

    Inicial

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/maps', function(req, res) {
+  var dados = [
+    {
+      lat: -25.470991, 
+      lon: -49.271036
+    },
+    {
+      lat: -0.935586,
+      lon: -49.635540
+    },
+    {
+      lat: -2.485874, 
+      lon: -43.128493
+    }
+  ];
+
+  res.send(JSON.stringify(dados));
+});
+
+app.get('/user', function(req,res){
+
+    var userInfo ={
+        nome:"Teste",
+        idade:29
+    }
+
+    res.send(JSON.stringify(userInfo));
+});
+
+
+app.listen(8000, function() {
+  console.log('Servidor rodando na porta 8000.');
+});
